b3387c367c2173057d97d879f2f3e0782d32983f
Noor Aloune Sagouma <nalosag@gmail.com>
2026-03-24 00:27:52 +0100

    feat(HomePage): add navigation button to AgentsPage

---
+++ src/app/pages/agents-page/agents-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agents-page',
+  imports: [],
+  templateUrl: './agents-page.html',
+  styleUrl: './agents-page.scss',
+})
+export class AgentsPage {}
