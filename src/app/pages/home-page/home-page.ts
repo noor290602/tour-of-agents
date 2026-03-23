252f129afbaa5e30436127d3a16663f11f0a4724
Noor Aloune Sagouma <nalosag@gmail.com>
2026-03-23 23:36:44 +0100

    feat(HomePage): data loaded on HomePage component using AgentService

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AgentsService } from '../../services/agents.service';
+import { Agent } from '../../models/agent';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
-export class HomePage {}
+export class HomePage {
+
+  agentService = inject(AgentsService);
+
+  agents = signal<Agent[]>([]);
+
+  ngOnInit() {
+    this.agentService.getAgents().subscribe((data) => {
+      
+      console.log("DATA:", data);
+
+      this.agents.set(data);
+    });
+  }
+}
