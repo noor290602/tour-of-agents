567ba68b5dd89cdeef452d5ddc6053cd69ffe5a8
Noor Aloune Sagouma <nalosag@gmail.com>
2026-03-24 00:04:46 +0100

    feat(HomePage): loaded general agents information

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,11 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AgentsService } from '../../services/agents.service';
 import { Agent } from '../../models/agent';
+import { KeyValuePipe } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
-  imports: [RouterLink],
+  imports: [RouterLink, KeyValuePipe],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
@@ -15,12 +16,25 @@ export class HomePage {
 
   agents = signal<Agent[]>([]);
 
-  ngOnInit() {
-    this.agentService.getAgents().subscribe((data) => {
-      
-      console.log("DATA:", data);
 
+ // 👉 Computed signals for metrics
+  totalAgents = computed(() => this.agents().length);
+
+  rolesCount = computed(() => {
+    const counts: Record<string, number> = {};
+    this.agents().forEach(a => {
+      if (a.role) {
+        counts[a.role] = (counts[a.role] || 0) + 1;
+      }
+    });
+    return counts;
+  });
+
+
+  ngOnInit() {
+    this.agentService.getAgents().subscribe(data => {
       this.agents.set(data);
     });
   }
+
 }
